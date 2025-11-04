7582fe7b839377f34b5a2e18b49da07ce8f91b2f
Shuhari <hello@nsshuhari.co>
2025-11-04 16:23:15 +0800

    Initial Commit

---
+++ Insights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insights
+//
+//  Created by Shuhari on 2025-11-04 16:23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
